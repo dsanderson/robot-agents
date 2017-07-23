49964ccaaeb9c8b3f0bda57655546c1fbd16035e
dsanderson <the.real.david.anderson@gmail.com>
2017-07-24 01:23:15 +0800

    almost...

--- design_agents.py
+++ design_agents.py
@@ -4,9 +4,10 @@ import utils
 class Wheel():
     """A simple wheel, that adds appropriate constraints to solve for the robot position.
     Links to parts that can provide a torque and/or speed"""
-    def __init__(self, reqs, radius, mass, parent):
+    def __init__(self, reqs, radius, mass, parent=None):
         self.parent = parent
         self.radius = radius
+        self.mass = mass
         self.terrain = parent.terrain
 
     def set_position(self, x, y):
@@ -29,27 +30,132 @@ class Wheel():
             self.free_forces.append(0) #angular force
         return self.free_forces
 
-    def convert_free_forces():
-
-
     def set_free_forces(self, forces):
         for i, f in enumerate(forces):
             self.free_forces[i] = f
 
-    def get_net_force_x(self):
-        forces = self.convert_free_forces()
+    def get_transmitted_force(self):
+        net_external_torque = 0
+        net_external_force_x = 0
+        net_external_force_y = -self.mass*9.8
+        for i, c in enumerate(self.contact_points):
+            ang = math.atan2(c[1]-self.y, c[0]-self.x)
+            #TODO check right hand rule
+            net_external_torque += math.sin(ang)*self.free_forces[2*i]*self.radius
+            net_external_torque += math.cos(ang)*self.free_forces[2*i+1]*self.radius
+            net_external_force_x += self.free_forces[2*i]
+            net_external_force_y += self.free_forces[2*i+1]
+        transmitted_force = [-net_external_force_x, -net_external_force_y, -net_external_torque]
+        return transmitted_force
 
-    def get_net_force_y(self):
-        pass
+    def get_net_forces(self):
+        """we assume each body is static, so we can set the net force to zero and calculate the corresponding internal forces"""
+        return [0,0,0]
 
-    def get_net_torque(self):
-        pass
+    def get_config_vars(self):
+        if self.parent==None:
+            return ["x","y"]
+        else:
+            return []
 
-    def get_config_vars():
+    def set_config_vars(self, vars):
+        self.set_position(vars[0], vars[1])
+
+class Motor():
+    def __init__(self, mass, max_torque, parent=None, child=None):
+        self.mass = mass
+        self.parent = parent
+        self.child = child
+        self.max_torque = max_torque
+
+    def get_config_vars(self):
         return []
 
+    def get_transmitted_force(self):
+        x, y, torque = *self.child.get_transmitted_force()
+        y = y-self.mass*9.8
+        if abs(torque)>abs(self.max_torque):
+            out_torque = self.signum(torque)*self.max_torque
+        else:
+            out_torque = torque
+        transmitted_force = [-x, -y, -out_torque]
+        return transmitted_force
+
+    def get_net_force(self):
+        tf = self.get_transmitted_force()
+        ef = self.child.get_transmitted_force()
+        forces = [sum(x) for x in zip(tf, ef)]
+        return forces
+
+    def set_position(self, x, y):
+        self.x = x
+        self.y = y
+        self.child.set_position(x,y)
 
 class Linkage():
-    """ """
+    """Class to generate and solve forces for linkages"""
+    #TODO: how do we handle linkage loops, ensuring the ends meet up? add a check/cost function:soln, ass a pin object
+    def __init__(self, length, mass, child, parent, is_root=False):
+        self.length = length
+        self.mass = mass
+        self.child = child
+        self.parent = parent
+        self.is_root = is_root
+        self.angle = 0
 
-class
+    def get_child_xy(self):
+        x = self.x+self.length*math.cos(self.angle)
+        y = self.y+self.length*math.sin(self.angle)
+        return x, y
+
+    def set_position(self, x, y):
+        self.x = x
+        self.y = y
+        self.child.set_position(*self.get_child_xy())
+
+    def get_config_vars(self):
+        if self.is_root==True:
+            return ["x","y","angle"]
+        else:
+            return ["angle"]
+
+    def set_config_vars(self, vars):
+        if self.is_root == True:
+            self.angle = vars[2]
+            self.set_position(vars[0], vars[1])
+        else:
+            self.angle = vars[0]
+            self.child.set_position(*self.get_child_xy())
+
+    def get_net_force(self):
+        return [0,0,0]
+
+    def get_transmitted_force(self):
+        x, y, torque = *self.child.get_transmitted_force()
+        net_torque = torque+math.sin(self.ang+math.pi)*x*self.length
+        net_torque += math.cos(self.ang+math.pi)*y*self.length
+        net_y = y-self.mass*9.8
+        net_torque += math.cos(self.ang+math.pi)*-self.mass*9.8*self.length*0.66
+        transmitted_force = [-x, -net_y, -net_torque]
+        return transmitted_force
+
+class Pin():
+    def __init__(self, parent1=None, parent2=None):
+        self.parent1 = parent1
+        self.parent2 = parent2
+
+    def get_config_vars(self):
+        return []
+
+    def get_transmitted_force(self):
+
+
+    def get_net_force(self):
+
+
+    def set_position(self, x, y):
+        self.x = x
+        self.y = y
+        self.child.set_position(x,y)
+
+    def score_config(self):
--- utils.py
+++ utils.py
@@ -6,5 +6,11 @@ def sample():
 def dist(p1,p2):
     return math.sqrt((p1[0]-p2[0])**2+(p1[1]-p2[1])**2)
 
+def signum(num):
+    if num>=0:
+        return 1
+    else:
+        return -1
+
 def walk_design(design):
     """walk down the .children of a design, returning each, as a generator"""
